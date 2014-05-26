a83bc31f9e6f611f05fa3f3c1be342d5044adb79
Hector Martin <marcan@pixiv.com>
2014-05-26 14:57:46 +0900

    Add test animation for unpacked mode

---
+++ test/hon-meta.js
@@ -0,0 +1,18 @@
+var hon_meta = {
+    frames: [
+        {file: "hon/000000.png", delay: 1000},
+        {file: "hon/000001.png", delay: 1000},
+        {file: "hon/000002.png", delay: 100},
+        {file: "hon/000003.png", delay: 400},
+        {file: "hon/000004.png", delay: 100},
+        {file: "hon/000005.png", delay: 100},
+        {file: "hon/000006.png", delay: 400},
+        {file: "hon/000007.png", delay: 100},
+        {file: "hon/000008.png", delay: 400},
+        {file: "hon/000009.png", delay: 100},
+        {file: "hon/000010.png", delay: 100},
+        {file: "hon/000011.png", delay: 800},
+        {file: "hon/000012.png", delay: 100},
+        {file: "hon/000013.png", delay: 1000},
+    ],
+}
